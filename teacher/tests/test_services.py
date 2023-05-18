585a2e802f310d94d275f0ab1a0c6f6bae3af466
EA2311 <andreyyerzhov@gmail.com>
2023-05-19 00:01:11 +0300

    test(teacher): add test for set_context function

---
+++ teacher/tests/test_services.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+from teacher.models import Classroom
+from teacher.services.models_services import get_random_string, classroom_file_name
+from teacher.services.set_context import set_context
+
+
+class SetContextTestCase(TestCase):
+
+    def test_set_context(self) -> None:
+        """Checks if custom set_context function return a dict with received values."""
+        self.assertDictEqual(set_context({}, pk=1, student=2), {'pk': 1, 'student': 2})
