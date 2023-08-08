cf97722ee99e27015bd949bf2622e1e8c24f0960
EA2311 <andreyyerzhov@gmail.com>
2023-08-08 20:15:58 +0300

    test(teacher.models): add an unit test for HomeTask model

--- teacher/tests/test_models.py
+++ teacher/tests/test_models.py
@@ -1,19 +1,19 @@
 from django.test import TestCase
 
 from accounts.models import Teacher, User
-from teacher.models import Classroom, Subject
+from teacher.models import Classroom, Subject, HomeTask
 
 
 class TeacherTestCase(TestCase):
     @classmethod
     def setUpTestData(cls) -> None:
         cls.user = User.objects.create(email='test@gmail.com',
-                                   phone_number='12412414214',
-                                   first_name='Fname',
-                                   last_name='Lname',
-                                   patronymic='Pname',
-                                   is_teacher=True
-                                   )
+                                       phone_number='12412414214',
+                                       first_name='Fname',
+                                       last_name='Lname',
+                                       patronymic='Pname',
+                                       is_teacher=True
+                                       )
 
         cls.teacher = Teacher.objects.create(user=cls.user)
 
@@ -23,6 +23,7 @@ class TeacherTestCase(TestCase):
                                                  teacher=cls.teacher
                                                  )
         cls.subject = Subject.objects.create(subject_name='Math', classroom=cls.classroom, image='1.png')
+        cls.home_task = HomeTask.objects.create(task='task', subject=cls.subject)
 
 
 class ClassroomModelTest(TeacherTestCase):
@@ -39,3 +40,11 @@ class SubjectModelTest(TeacherTestCase):
         subject = self.subject
         expected_object_name = subject.subject_name
         self.assertEqual(expected_object_name, str(subject))
+
+
+class HomeTaskModelTest(TeacherTestCase):
+
+    def test_hometask_object_name_is_class_name_field(self):
+        home_task = self.home_task
+        expected_object_name = f'{home_task.task} - {home_task.pub_date}'
+        self.assertEqual(expected_object_name, str(home_task))
