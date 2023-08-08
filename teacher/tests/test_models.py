85bbe5c2074a9c5dd8d95fb60663c112b3962e51
EA2311 <andreyyerzhov@gmail.com>
2023-08-08 20:11:18 +0300

    test(teacher.models): add an unit test for Subject model

--- teacher/tests/test_models.py
+++ teacher/tests/test_models.py
@@ -1,31 +1,41 @@
 from django.test import TestCase
 
 from accounts.models import Teacher, User
-from teacher.models import Classroom
+from teacher.models import Classroom, Subject
 
 
-class ClassroomModelTest(TestCase):
+class TeacherTestCase(TestCase):
     @classmethod
     def setUpTestData(cls) -> None:
-        user = User.objects.create(email='test1@gmail.com',
-                                   phone_number='12d412414214',
+        cls.user = User.objects.create(email='test@gmail.com',
+                                   phone_number='12412414214',
                                    first_name='Fname',
                                    last_name='Lname',
                                    patronymic='Pname',
                                    is_teacher=True
                                    )
 
-        teacher = Teacher.objects.create(user=user)
+        cls.teacher = Teacher.objects.create(user=cls.user)
 
         cls.classroom = Classroom.objects.create(class_name='1-A',
-                                                 key='aasdsfssafsffadvfaefsca',
+                                                 key='asdsfssafsffadvfaefsca',
                                                  image='img.png',
-                                                 teacher=teacher
+                                                 teacher=cls.teacher
                                                  )
+        cls.subject = Subject.objects.create(subject_name='Math', classroom=cls.classroom, image='1.png')
+
+
+class ClassroomModelTest(TeacherTestCase):
 
     def test_classroom_object_name_is_class_name_field(self):
-        classroom = ClassroomModelTest.classroom
+        classroom = self.classroom
         expected_object_name = classroom.class_name
         self.assertEqual(expected_object_name, str(classroom))
 
 
+class SubjectModelTest(TeacherTestCase):
+
+    def test_subject_object_name_is_class_name_field(self):
+        subject = self.subject
+        expected_object_name = subject.subject_name
+        self.assertEqual(expected_object_name, str(subject))
