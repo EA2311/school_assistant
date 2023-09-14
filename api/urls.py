cf5d93796478dca63c598b8b9bca5975d798b9f2
EA2311 <andreyyerzhov@gmail.com>
2023-09-14 21:57:19 +0300

    feat(api): add app_name for api.urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from api.views import TeacherList, TeacherDetail
 
+app_name = 'api'
+
 urlpatterns = [
     path('teachers/', TeacherList.as_view(), name='teacher-list'),
     path('teachers/<int:pk>/', TeacherDetail.as_view(), name='teacher-detail'),
